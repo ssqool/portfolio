07719f37dd18c4817e2a0addb3c8bd31cdb61770
Sergiy Skuleba <56043727+ssqool@users.noreply.github.com>
2020-11-29 16:21:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-// $(document).ready(function() {
-//     $('.header-burger').click(function (event) {
-//         $('.header-burger,.header-menu').toggleClass('active');
-//     });
-// });
-
-let get=(id)=>{
-    return document.querySelector(id)
-}
-
-get('.header-burger').addEventListener('click', showMenu);
-
-get('.header-burger').addEventListener('touchstart', showMenu);
-
-function showMenu(){
-    get('.header-burger').classList.add('active');
-    get('.header-menu').classList.add('active');
-    if(get('.header-burger').classList.contains('acitve')){
-        get('.header-burger').classList.remove('active');
-        get('.header-menu').classList.remove('active');
-    }
-}
+$(document).ready(function() {
+  $('.header-burger').click(function (event) {
+    $('.header-burger,.header-menu').toggleClass('active');
+  });
+});
